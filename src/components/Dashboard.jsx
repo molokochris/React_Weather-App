a410ac4cb12ed5eec6322ec4d7ff3cf00a07156b
molokochris <molokochrisp742@gmail.com>
2023-08-04 18:59:05 +0200

    new css

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,6 +10,7 @@ import { LiaMapSolid, LiaMapPinSolid } from "react-icons/lia";
 import { RiApps2Line } from "react-icons/ri";
 import { WiCloud, WiStormShowers } from "react-icons/wi";
 import WeatherIcons from "./WeatherIcons";
+import logo from '../../public/SkyBits.png'
 
 export default function Dashboard(props) {
   const { list, city } = props.weather;
@@ -19,81 +20,14 @@ export default function Dashboard(props) {
 
   return (
     <div className="dashboard-container">
-      <div className="dashboard">
-        <div className="side-menu">
+      <div className="image-container"></div>
+      <div className="info-container">
+        <div className="current">
           <div className="logo">
-            <FaCloudsmith
-              style={{ fontSize: "x-large", color: "rgba(99, 99, 99, 0.8)" }}
-            />
-            <span
-              className="logo-text"
-              style={{
-                fontFamily: "Concert one, cursive ",
-                color: "rgba(250, 250, 250, 0.6)",
-              }}
-            >
-              SkyBits
-            </span>
-          </div>
-          <div className="side-items">
-            <ul style={{ listStyle: "none", padding: "0" }}>
-              <li>
-                <RiApps2Line />
-              </li>
-              <li>
-                <GiNestedEclipses />
-              </li>
-              <li>
-                <LiaMapSolid />
-              </li>
-              <li>
-                <SlCalender />
-              </li>
-              <li>
-                <BsGear />
-              </li>
-            </ul>
+            <img src={logo} alt="" />
           </div>
         </div>
-        <div className="main-menu">
-          <div className="rw-1">
-            <div className="col-1">
-              <div className="main-info">
-                <i className="wi wi-day-sunny img"></i>
-                <span className="temp">{Math.floor(list[0].main.temp)}&deg;C</span>
-                <div className="i-sp">
-                  <WiStormShowers style={{ fontSize: "xx-large" }} />
-                  <span style={{ fontSize: "medium" }}>
-                    {list[0].weather[0].description}
-                  </span>
-                </div>
-              </div>
-              <div className="extra-info">
-                <div className="i-sp">
-                  <LiaMapPinSolid className="icons" />
-                  <span>
-                    {city.name}, {city.country}
-                  </span>
-                </div>
-                <div className="i-sp">
-                  <SlCalender className="icons" />
-                  <span>23 July 2023 5:23 AM</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-2">Today's Highlight</div>
-          </div>
-          <div className="rw-2">
-            <div className="rw-2a">
-              <div className="col-1 s-col">7 days Forecast</div>
-              <div className="col-2 s-col">Weather condition map</div>
-            </div>
-            <div className="rw-2b">
-              <div className="col-1">col-1</div>
-              <div className="col-2">col-2</div>
-            </div>
-          </div>
-        </div>
+        <div className="more-info">2</div>
       </div>
     </div>
   );
